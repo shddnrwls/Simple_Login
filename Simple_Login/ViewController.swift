1a1c02c30eb5e384a4cfeba9a62c00fa299989be
shddnrwls <shddnrwls@gmail.com>
2020-05-26 17:59:16 +0900

    create project

---
+++ Simple_Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple_Login
+//
+//  Created by mac on 2020/05/26.
+//  Copyright © 2020 UkJin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
